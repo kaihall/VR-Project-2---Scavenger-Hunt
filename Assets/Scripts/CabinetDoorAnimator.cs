d820e11595d0aec23c0c332e93d49a74320ac8ac
kaihall <49812013+kaihall@users.noreply.github.com>
2021-03-09 15:39:05 -0500

    Scene changes. Opening and closing doors.
    Moved objects around so that the player can see inside the fridge. Added material changes on hover to interactable objects. Added fridge/cabinet opening/closing on button press.

---
+++ Assets/Scripts/CabinetDoorAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinetDoorAnimator : MonoBehaviour
+{
+    private bool closed = true;
+	public GameObject cabinet;
+	
+	public void ChangeState() {
+		if (closed) {
+			closed = false;
+			cabinet.GetComponent<Animator>().Play("Base Layer.KS_Open");
+		} else {
+			closed = true;
+			cabinet.GetComponent<Animator>().Play("Base Layer.KS_Close");
+		}
+	}
+}
---
+++ Assets/Scripts/FridgeDoorAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeDoorAnimator : MonoBehaviour
+{
+    private bool closed = true;
+	public GameObject fridge;
+	
+	public void ChangeState() {
+		if (closed) {
+			closed = false;
+			fridge.GetComponent<Animator>().Play("Base Layer.F_Open");
+		} else {
+			closed = true;
+			fridge.GetComponent<Animator>().Play("Base Layer.F_Close");
+		}
+	}
+}
