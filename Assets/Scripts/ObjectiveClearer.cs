eb35cdc6b57bcb2ba175763761d634b2396aada2
kaihall <49812013+kaihall@users.noreply.github.com>
2021-03-09 21:08:28 -0500

    Objective Clearer
    Added script that clears objectives. Also added an audio source.

---
+++ Assets/Scripts/ObjectiveClearer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveClearer : MonoBehaviour
+{
+    public GameObject objective;
+	public AudioSource clearMusic;
+	
+	void Start() {
+		clearMusic = GetComponent<AudioSource>();
+	}
+	
+	public void ClearObjective() {
+		objective.SetActive(false);
+		clearMusic.Play();
+	}
+}
